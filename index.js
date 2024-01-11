2145702ac6ac4c0535eb772e359cc53eb86e77dd
Mina-Mikhael <mina.s.mikhael@gmail.com>
2024-01-10 23:00:10 -0500

    installing dep.

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+const express = require("express");
+const server = express();
+const port = 8080;
+const mongoose = require("mongoose");
 
-console.log("Hello World!");
+server.use(express.json());
+
+mongoose
+  .connect("mongodb://localhost:27017/baitShop")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB");
+    console.log(err);
+  });
+
+server.get("/fish", (req, res) => {
+  res.send("Hello World");
+});
+
+server.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
